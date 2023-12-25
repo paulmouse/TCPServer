bf3fa25ecc6e1d28f76510f7f18e8daca834b51c
p.teperik <p.teperik@yandex.ru>
2023-12-25 13:23:25 +0300

    v1.2

--- TCPServer.py
+++ TCPServer.py
@@ -1,16 +1,12 @@
 import socket
 import datetime
 import configparser
+import threading
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 #hostIp = config.get('settings', 'hostip')
 #port = int(config.get('settings', 'port'))
-
-def get_local_ip():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    return local_ip
 
 def datasplit(pos, data):
     relays = {'16': 1, '17': 2, '18': 3, '19': 4, '20': 5, '21': 6, '22': 7, '23': 8, '255': 'ON', '0': 'OFF',
@@ -37,6 +33,41 @@ def analogdata(pos, datastrHex):
         return pt100Value
     except:
         return ''
+
+def handle_client(conn, addr):
+    ip_address = addr[0]
+    print(f"Подключен клиент {ip_address}")
+    with open('tcp_requests.log', 'a') as log_file:
+        log_file.write(f"{datetime.datetime.now()}\tConnected\t{ip_address}\n")
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            bytes = list(data)
+            inputType = datasplit(1, data)
+            relayNo = datasplit(3, data)
+            datastrHex = bytearray(bytes).hex()
+            stateVal = datasplit(4, data)
+            analogState = analogdata(6, datastrHex)
+            if not data:
+                print(f"Подключение клиента с {ip_address} закрыто")
+                break
+            if inputType == 'DI':
+                print(f"Цифровой вход: {relayNo} Статус: {stateVal}")
+            else:
+                print(f"Аналоговый вход: 1 Значение: {analogState}");
+            # print(f"Тип входа: {inputType}")
+            with open('tcp_requests.log', 'a') as log_file:
+                if inputType == 'DI':
+                    log_file.write(f"{datetime.datetime.now()}\tInput\t{ip_address}\tDigital\t{relayNo}\t{stateVal}\n")
+                else:
+                    log_file.write(f"{datetime.datetime.now()}\tInput\t{ip_address}\tAnalog\t1\t{analogState}\n")
+
+def get_local_ip():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return local_ip
+
+
 def main():
     HOST = get_local_ip() # Принимаем подключения со всех интерфейсов
     PORT = int(config.get('settings', 'port'))
@@ -48,33 +79,10 @@ def main():
         print(f"Сервер запущен по адресу {localIp} и ожидает подключений на порту {PORT}...")
         with open('tcp_requests.log', 'a') as log_file:
             log_file.write(f"{datetime.datetime.now()}\tStart\n")
-        conn, addr = s.accept()
-        with conn:
-            ip_address = addr[0]
-            print(f"Подключен клиент {ip_address}")
-            with open('tcp_requests.log', 'a') as log_file:
-                log_file.write(f"{datetime.datetime.now()}\tConnected\t{ip_address}\n")
-            while True:
-                data = conn.recv(1024)
-                bytes = list(data)
-                inputType = datasplit(1, data)
-                relayNo = datasplit(3,data)
-                datastrHex = bytearray(bytes).hex()
-                stateVal = datasplit(4,data)
-                analogState = analogdata(6, datastrHex)
-                if not data:
-                    break
-                #print(f"Данные от {ip_address}: {bytes}")
-                #print(f"Данные от {ip_address}: {datastrHex}")
-                if inputType == 'DI':
-                     print(f"Цифровой вход: {relayNo} Статус: {stateVal}")
-                else: print(f"Аналоговый вход: 1 Значение: {analogState}");
-               # print(f"Тип входа: {inputType}")
-                with open('tcp_requests.log', 'a') as log_file:
-                    if inputType == 'DI':
-                        log_file.write(f"{datetime.datetime.now()}\tInput\t{ip_address}\tDigital\t{relayNo}\t{stateVal}\n")
-                    else:
-                        log_file.write(f"{datetime.datetime.now()}\tInput\t{ip_address}\tAnalog\t1\t{analogState}\n")
+        while True:
+            conn, addr = s.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
 
 if __name__ == "__main__":
     main()
